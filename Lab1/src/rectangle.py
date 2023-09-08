355e9ec90288f819fc0a6a88c9287f7bfaa35ff4
Xiyu-Serena-Wan <64742214+Xiyu-Serena-Wan@users.noreply.github.com>
2023-09-07 20:57:04 -0700

    update rectangle.py

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -5,13 +5,27 @@
  Note that there is no constructor or destructor,
  so a default one will be created for us.
 """
-class Rectangle:
+
+
+class Shape:
     def set_values(self, x, y):
-        self.width = x
-        self.height = y
+        pass
 
     def area(self):
-        return self.width * self.height
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    def set_values(self, x, y):
+        self._width = x
+        self._height = y
+
+    def area(self):
+        return self._width * self._height
 
 
 if __name__ == "__main__":
